71f0b643a6597857e179526f7d10d73040c44dfe
Patti85 <pbeier24@googlemail.com>
2021-03-14 19:14:54 +0100

    moving the cube

--- script.js
+++ script.js
@@ -9,17 +9,29 @@ document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0xff69b4 });
-const size = 100;
-const divisions = 100;
 
- 
+var xPos = 0.1;
+
+
+
 
 const cube = new THREE.Mesh(geometry, material);
- 
+
 scene.add(cube);
 
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+
+    if (keyCode == 37) { //left arrow
+        cube.position.x -= xPos;
+    } else if (keyCode == 39) { //right arrow
+        cube.position.x += xPos;
+    }
+};
+
 cube.rotation.x += 0.6;
- 
+
 const edges = new THREE.EdgesGeometry(geometry);
 const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
 cube.add(line);
@@ -28,13 +40,13 @@ camera.position.z = 5;
 camera.rotation.x = 90 * Math.PI / 180
 camera.lookAt(scene.position)
 
-const axesHelper = new THREE.AxesHelper( );
-scene.add( axesHelper );
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 const animate = function () {
     requestAnimationFrame(animate);
     cube.position.z -= 0.1;
-    camera.position.z -= 0.2;
+    camera.position.z -= 0.1;
     renderer.render(scene, camera);
 };
 

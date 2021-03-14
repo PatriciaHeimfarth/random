43c30ecee9bfde6a4335eeff74790d454d809829
Patti85 <pbeier24@googlemail.com>
2021-03-14 22:09:32 +0100

    clean up

---
+++ cube.js
@@ -0,0 +1,1 @@
+const cube = new THREE.Mesh(geometry, material);
--- script.js
+++ script.js
@@ -1,54 +1,15 @@
 import * as THREE from "../node_modules/three/build/three.module.js"
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+const planeWidth = 5;
 const renderer = new THREE.WebGLRenderer();
+let plane;
+let cube;
+let camera;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0xff69b4 });
-
-var xPos = 0.1;
-
-
-const planeWidth = 5;
-
-const cube = new THREE.Mesh(geometry, material);
-
-
-
-document.addEventListener("keydown", onDocumentKeyDown, false);
-function onDocumentKeyDown(event) {
-    var keyCode = event.which;
-
-    if (keyCode == 37 &&  cube.position.x > -planeWidth / 2) { //left arrow
-        cube.position.x -= xPos;
-    } else if (keyCode == 39 &&  cube.position.x < planeWidth / 2) { //right arrow
-        cube.position.x += xPos;
-    }
-};
-
-cube.rotation.x += 0.6;
-
-const planeGeometry = new THREE.PlaneGeometry( planeWidth, 200, 32 );
-const geoMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-const plane = new THREE.Mesh( planeGeometry, geoMaterial );
-
-scene.add( plane );
-plane.add(cube);
-
-const edges = new THREE.EdgesGeometry(geometry);
-const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
-cube.add(line);
-
-camera.position.z = 5;
-camera.rotation.x = 90 * Math.PI / 180
-camera.lookAt(scene.position)
-
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
 
 const animate = function () {
     requestAnimationFrame(animate);
@@ -56,5 +17,53 @@ const animate = function () {
     camera.position.y += 0.01;
     renderer.render(scene, camera);
 };
+
+const initCube = function () {
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0xff69b4 });
+    cube = new THREE.Mesh(geometry, material);
+    cube.rotation.x += 0.6;
+    plane.add(cube);
+    const edges = new THREE.EdgesGeometry(geometry);
+    const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
+    cube.add(line);
+}
+
+const initPlane = function () {
+    const planeGeometry = new THREE.PlaneGeometry(planeWidth, 200, 32);
+    const geoMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
+    plane = new THREE.Mesh(planeGeometry, geoMaterial);
+
+    scene.add(plane);
+}
+
+const initCamera = function () {
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+    camera.rotation.x = 90 * Math.PI / 180
+    camera.lookAt(scene.position)
+}
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    let keyCode = event.which;
+    const xPos = 0.1;
+    if (keyCode == 37 && cube.position.x > -planeWidth / 2) { //left arrow
+        cube.position.x -= xPos;
+    } else if (keyCode == 39 && cube.position.x < planeWidth / 2) { //right arrow
+        cube.position.x += xPos;
+    }
+};
+initPlane();
+initCube();
+initCamera();
+
+
+
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
+
+
 
 animate();
